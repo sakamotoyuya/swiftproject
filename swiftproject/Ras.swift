80543ac895f11302bea09ab122ad927d6ef4c8ca
sakamotoyuya <will-wish@docomo.ne.jp>
2019-03-24 22:32:11 +0900

    Rasクラス追加

---
+++ swiftproject/Ras.swift
@@ -0,0 +1,13 @@
+//
+//  Ras.swift
+//  swiftproject
+//
+//  Created by sakamotoyuya on 2019/03/24.
+//  Copyright © 2019 sakamotoyuya. All rights reserved.
+//
+
+import UIKit
+
+class Ras: NSObject {
+
+}
